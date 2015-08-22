26c3d0f403ddb4292076eb0c0babc11709c6b6f5
Alexander I.Grafov <grafov@gmail.com>
2015-08-22 15:55:27 +0300

    Add commentary for the package

--- go-playground.el
+++ go-playground.el
@@ -3,6 +3,7 @@
 ;; Copyright (C) 2015  Alexander I.Grafov (axel)
 
 ;; Author: Alexander I.Grafov (axel) <grafov@gmail.com>
+;; URL: https://github.com/grafov/go-playground
 ;; Keywords: tools, golang
 
 ;; This program is free software; you can redistribute it and/or modify
@@ -19,6 +20,18 @@
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
+
+;; Local playground for the Go programs similar to play.golang.org.
+;; `M-x go-playground` and type you golang code then make&run it with `C-Return`.
+
+;; Playground works around `go-mode` and requires preconfigured environment
+;; for Go language.
+
+;; I recommend you to use `goimports` instead of `gofmt` for automatically make
+;; import clauses. It very comfortable especially for experimenting with code
+;; in playground.
+
+;; You may push code to play.golang.org with go-mode' function `go-play-buffer`.
 
 ;; 
 
@@ -93,4 +106,4 @@ func main() {
 
 
 (provide 'go-playground)
-;;; goplay-mode.el ends here
+;;; go-playground.el ends here
